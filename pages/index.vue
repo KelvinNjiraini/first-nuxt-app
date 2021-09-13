aeb49179d8ff51bbeae16e5b50d62c908824d7c1
Kelvin Njiraini <njirainikelvin@gmail.com>
2021-09-13 18:50:40 +0300

    layouts and nested pages

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,8 @@
 <template>
-  <Tutorial/>
+  <section>
+    <h1>Hello, This is just a test</h1>
+    <nuxt-link to="/users">Users page</nuxt-link>
+  </section>
 </template>
 
 <script>
---
+++ pages/users/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <h1>Users page</h1>
+</template>
+
+<script>
+export default {
+  layout: 'users',
+}
+</script>
